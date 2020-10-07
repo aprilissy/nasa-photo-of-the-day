2831a65739bd7a321e0cd6694ec278b89f549485
April Darger <april.m.darger@gmail.com>
2020-10-07 16:41:30 -0600

    Set up my wireframed site with relevant info and image.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios'
+import DisplayAPOD from './components/DisplayAPOD/DisplayAPOD'
 
 function App() {
-  const [nasaAPOD, setNasaAPOD] = useState([])
+  const [nasaAPOD, setNasaAPOD] = useState({})
 
   useEffect(() => {
     const fetchNasaAPOD = () => {
       axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
         .then(res => {
           setNasaAPOD(res.data)
-          console.log("fetchNasaAPOD -> res.data", res.data)
+          // console.log("fetchNasaAPOD -> res.data", res.data)
 
         })
         .catch(err => {
@@ -18,12 +19,14 @@ function App() {
         })
     }
     fetchNasaAPOD()
+
   }, [])
+  console.log('sup I am data', nasaAPOD)
 
   return (
     <div className="App">
       <h1>Let's take a look at NASA's EPIC Astronomy Photo of the Day!</h1>
-      <h2>Photo date: {nasaAPOD.date}</h2>
+      <DisplayAPOD nasaAPOD={nasaAPOD} />
     </div>
   );
 }
