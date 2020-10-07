342e56bfdcaee294947966ba964826110957bd31
April Darger <april.m.darger@gmail.com>
2020-10-07 14:43:50 -0600

    Getting the project set up by adding useState and useEffect hooks, pullling in NASA APOD data using axios.get, console logging to check, etc...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  const [nasaAPOD, setNasaAPOD] = useState([])
+
+  useEffect(() => {
+    const fetchNasaAPOD = () => {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(res => {
+          setNasaAPOD(res.data)
+          console.log("fetchNasaAPOD -> res.data", res.data)
+
+        })
+        .catch(err => {
+          debugger
+        })
+    }
+    fetchNasaAPOD()
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Let's take a look at NASA's EPIC Astronomy Photo of the Day!</h1>
+      <h2>Photo date: {nasaAPOD.date}</h2>
     </div>
   );
 }
